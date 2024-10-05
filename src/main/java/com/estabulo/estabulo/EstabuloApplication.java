4eaa1a450c17e1ee0d6bdaf06f0da79bf9045d83
Edson Correia <edsoncorreia997@gmail.com>
2024-10-05 19:37:48 -0300

    Criação do Projeto.

---
+++ src/main/java/com/estabulo/estabulo/EstabuloApplication.java
@@ -0,0 +1,13 @@
+package com.estabulo.estabulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstabuloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstabuloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estabulo/estabulo/EstabuloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estabulo.estabulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstabuloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
