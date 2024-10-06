c0af598bc809f8b94fedd2ae42353a96dfcf728d
Edson Correia <edsoncorreia997@gmail.com>
2024-10-05 23:12:08 -0300

    Criação classe: Provisao

--- src/main/java/com/estabulo/estabulo/model/Provisao.java
+++ src/main/java/com/estabulo/estabulo/model/Provisao.java
@@ -1,4 +1,46 @@
 package com.estabulo.estabulo.model;
 
-public class Provisao {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity (name="Provisao")
+public class Provisao implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name="Id")
+    private int id;
+
+    @Column (name="Nome")
+    private String nome;
+
+    @Column (name="Tipo") //comida, estimulante, ferramenta ou suplemento
+    private String tipo;
+
+    @Column (name="Preço")
+    private float preco;
+
+    //definitivamente não sei se
+    //mais colunas serão necessáriasKKKKK
+
+
+    public Provisao() {
+    }
+
+    public Provisao(String nome, String tipo, float preco) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Provisao{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
 }
